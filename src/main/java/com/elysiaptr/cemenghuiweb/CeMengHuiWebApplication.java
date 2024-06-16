1fc3564b6a85e16f70d435f3cdefbb9035cc9595
ElysiaPtr <elysiaptr@qq.com>
2024-06-16 18:51:59 +0800

    init

---
+++ src/main/java/com/elysiaptr/cemenghuiweb/CeMengHuiWebApplication.java
@@ -0,0 +1,13 @@
+package com.elysiaptr.cemenghuiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeMengHuiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeMengHuiWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elysiaptr/cemenghuiweb/CeMengHuiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elysiaptr.cemenghuiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeMengHuiWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
