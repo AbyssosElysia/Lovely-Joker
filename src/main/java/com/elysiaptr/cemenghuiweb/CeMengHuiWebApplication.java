b1f1a682659c0291d97f16f1debb09a511a292a1
ElysiaPtr <elysiaptr@qq.com>
2024-06-22 14:04:28 +0800

    更新主类

--- src/main/java/com/elysiaptr/cemenghuiweb/CeMengHuiWebApplication.java
+++ src/main/java/com/elysiaptr/cemenghuiweb/CeMengHuiWebApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CeMengHuiWebApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(CeMengHuiWebApplication.class, args);
     }
 
